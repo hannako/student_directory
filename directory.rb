0a4f00e5b570a4a001624554f4a4b2f665058be9
hannako <jessjones1@gmail.com>
2016-06-21 19:20:40 +0100

    update to the input_students method 8.Exercises:7. Added checks for typos in cohort spelling. Changed cohort value to a symbol instead of a string.

--- directory.rb
+++ directory.rb
@@ -14,44 +14,58 @@
 #########################################################
 
 def input_students
-  students = []
+students = []
 answer = ''
-until answer == 'no'
+
+until add_more = "no"
 
   puts "Please enter the name of the student"
     name = gets.chomp
 
-  puts "Which cohort is #{name} joining?"
-    cohort = gets.chomp
+cohort = ''
+  loop do
+    puts "Which cohort is #{name} joining?"
+    cohort = gets.chomp.capitalize.to_sym
+      break if (cohort == :January || cohort == :February || cohort == :March || cohort == :April || cohort == :May || cohort == :June || cohort == :July || cohort == :August || cohort == :September || cohort == :October || cohort == :November || cohort == :December)
+      puts "Sorry, I didn't get that. Please write the name of the month in full."
+    end
 
-  puts "On campus or online?"
-    course = gets.chomp
+puts "On campus or online?"
 
-  puts "What is #{name}'s age?"
+  course = gets.chomp
+
+puts "What is #{name}'s age?"
     age = gets.chomp
 
-  puts "Does #{name} have any coding experience?"
+puts "Does #{name} have any coding experience?"
     experience = gets.chomp
 
-    students << {name: name, cohort: cohort, course: course, age: age, experience: experience}
+students << {name: name, cohort: cohort, course: course, age: age, experience: experience}
 
   puts "Would you like to add more students? yes or no?"
-      answer = gets.chomp
+      add_more = gets.chomp.downcase
 
 end
       puts "Now we have #{students.count} students"
 
 students
+
 end
+
+
+################################################################
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
+################################################################
+
 def print_with_each(students)
   if students.empty?
     puts "No data to print"
+
   else
     width = 30
     puts "Name".ljust(width) + "Cohort".center(width) + "Details".rjust(width)
@@ -61,19 +75,28 @@ def print_with_each(students)
 end
 end
 
+################################################################
+
 def print_with_until(students)
   n = students.length
   count = 1
   until count == n
   puts "#{count}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
   count +=1
+  end
 end
-end
+
+################################################################
 
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
+
+
+
+
+################################################################
 
 def print_selection(letter,students)
   selection = []
@@ -90,6 +113,8 @@ def print_selection(letter,students)
     end
   end
 
+################################################################
+
 def print_shorts(students)
   shorts = []
   students.each do |student|
@@ -104,7 +129,11 @@ def print_shorts(students)
     end
   end
 
+
+
 students = input_students
+print_with_each(students)
 print_header
 print_with_each(students)
 print_footer(students)
+print_selection("k",students)
