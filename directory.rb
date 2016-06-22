59413040f1a15567ab1f1323af6dfa75d2444312
hannako <jessjones1@gmail.com>
2016-06-22 19:07:33 +0100

    Adding an exit command if no users are added 8.Exercise 12

--- directory.rb
+++ directory.rb
@@ -21,58 +21,59 @@ until add_more == "no"
 
   puts "Please enter the name of the student"
     name = gets.delete!("\n")
-    if name == ""
-      name = "tbc"
-    end
+if name == ""
+    puts "No students entered"
+    exit
 
 
-cohort = ''
-  loop do
-    puts "Which cohort is #{name} joining?"
-    cohort = gets.chomp.capitalize.to_sym
-      break if (cohort == :January || cohort == :February || cohort == :March || cohort == :April || cohort == :May || cohort == :June || cohort == :July || cohort == :August || cohort == :September || cohort == :October || cohort == :November || cohort == :December)
-      puts "Sorry, I didn't get that. Please write the name of the month in full."
-    end
-
-puts "On campus or online?"
-
-  course = gets.delete!("\n")
-  if course == ""
-     course = "tbc"
-  end
-
-
-puts "What is #{name}'s age?"
-    age = gets.delete!("\n")
-  if age == ""
-    age = "tbc"
-  end
-
-puts "Does #{name} have any coding experience?"
-    experience = gets.delete!("\n")
-    if experience == ""
-      experience = "tbc"
-    end
-
-students << {name: name, cohort: cohort, course: course, age: age, experience: experience}
-
-  # students.each do |student|
-    # student.each_pair do |key,value|
-      # if value == ""
-        # value = "tbc"
-      # end
-    # end
-  # end
-
-  loop do
-      puts "Would you like to add more students? yes or no?"
-      add_more = gets.delete!("\n").downcase
-      break if add_more == "yes" || add_more == "no"
-      puts "please answer yes or no."
+else
+    cohort = ''
+    loop do
+      puts "Which cohort is #{name} joining?"
+      cohort = gets.chomp.capitalize.to_sym
+        break if (cohort == :January || cohort == :February || cohort == :March || cohort == :April || cohort == :May || cohort == :June || cohort == :July || cohort == :August || cohort == :September || cohort == :October || cohort == :November || cohort == :December)
+        puts "Sorry, I didn't get that. Please write the name of the month in full."
       end
 
-      puts "Now we have #{students.count} students"
+  puts "On campus or online?"
 
+    course = gets.delete!("\n")
+    if course == ""
+       course = "tbc"
+    end
+
+
+  puts "What is #{name}'s age?"
+      age = gets.delete!("\n")
+    if age == ""
+      age = "tbc"
+    end
+
+  puts "Does #{name} have any coding experience?"
+      experience = gets.delete!("\n")
+      if experience == ""
+        experience = "tbc"
+      end
+
+  students << {name: name, cohort: cohort, course: course, age: age, experience: experience}
+
+    # students.each do |student|
+      # student.each_pair do |key,value|
+        # if value == ""
+          # value = "tbc"
+        # end
+      # end
+    # end
+
+    loop do
+        puts "Would you like to add more students? yes or no?"
+        add_more = gets.delete!("\n").downcase
+        break if add_more == "yes" || add_more == "no"
+        puts "please answer yes or no."
+        end
+
+        puts "Now our student count is #{students.count}"
+  end
 end
 students
 
